c591690aa7f14a409c3c5e69dbce57f9e05fa7fe
Hunt Zhan <huntzhan.dev@gmail.com>
2022-09-02 23:52:57 +0800

    Inject license header. (#44)
    * Inject license header to vkit and tests.
    
    * lint.

---
+++ vkit/__init__.py
@@ -0,0 +1,13 @@
+# Copyright 2022 vkit-x Administrator. All Rights Reserved.
+#
+# This project (vkit-x/vkit) is dual-licensed under commercial and SSPL licenses.
+#
+# The commercial license gives you the full rights to create and distribute software
+# on your own terms without any SSPL license obligations. For more information,
+# please see the "LICENSE_COMMERCIAL.txt" file.
+#
+# This project is also available under Server Side Public License (SSPL).
+# The SSPL licensing is ideal for use cases such as open source projects with
+# SSPL distribution, student/academic purposes, hobby projects, internal research
+# projects without external distribution, or other projects where all SSPL
+# obligations can be met. For more information, please see the "LICENSE_SSPL.txt" file.
